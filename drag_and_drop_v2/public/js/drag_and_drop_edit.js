26c3078d7191f96ec7dd79ebdd0b0586519bee38
Barry Paneer <barrypaneer@Barry-MacBook-Pro-4-Working.local>
2023-07-13 17:02:11 +0800

    Fixed: Quentin LUONG VAN ::: V_1 ::: Page 8 / 9

--- drag_and_drop_v2/public/js/drag_and_drop_edit.js
+++ drag_and_drop_v2/public/js/drag_and_drop_edit.js
@@ -803,6 +803,26 @@ function DragAndDropEditBlock(runtime, element, params) {
                         if (e.currentTarget.classList.contains('fa-caret-down')) {
                             e.currentTarget.classList.remove('fa-caret-down');
                             e.currentTarget.classList.add('fa-caret-up');
+
+                            let dropdown_content_elements = e.currentTarget.parentElement.getElementsByClassName('answer_zones_dropdown_content');
+
+                            if (dropdown_content_elements.length === 1) {
+                                let container_top = document.getElementById('id_answers_collection').offsetTop;
+                                let container_height = document.getElementById('id_answers_collection').clientHeight;
+                                let button_top = e.currentTarget.offsetTop;
+                                let button_height = e.currentTarget.clientHeight;
+                                let dropdown_content_element = dropdown_content_elements[0];
+                                let menu_height = dropdown_content_element.clientHeight;
+
+                                if ((button_top - container_top + button_height + menu_height) >= container_height) {
+                                    dropdown_content_element.style.top = e.currentTarget.offsetTop - 140 + "px";
+                                    dropdown_content_element.style.left = e.currentTarget.offsetLeft - e.currentTarget.nextElementSibling.clientWidth + e.currentTarget.clientWidth + 8 + "px";
+                                } else {
+                                    dropdown_content_element.style.top = e.currentTarget.offsetTop + 30 + "px";
+                                    dropdown_content_element.style.left = e.currentTarget.offsetLeft - e.currentTarget.nextElementSibling.clientWidth + e.currentTarget.clientWidth + 8 + "px";
+                                }
+
+                            }
                         }
                     });
                     $('.answer_zones_dropdown_content').bind('mouseleave', function(e) {
@@ -888,23 +908,25 @@ function DragAndDropEditBlock(runtime, element, params) {
                     });
 
                     $element.find('.answer_item').bind('mouseover', function(e) {
-                        let dropdown_content_elements = e.currentTarget.getElementsByClassName('answer_zones_dropdown_content');
-
-                        if (dropdown_content_elements.length === 1) {
-                            let container_top = document.getElementById('id_answers_collection').offsetTop;
-                            let container_height = document.getElementById('id_answers_collection').clientHeight;
-                            let button_top = e.currentTarget.offsetTop;
-                            let button_height = e.currentTarget.clientHeight;
-                            let dropdown_content_element = dropdown_content_elements[0];
-                            let menu_height = dropdown_content_element.clientHeight;
-
-                            if ((button_top - container_top + button_height + menu_height) >= container_height) {
-                                dropdown_content_element.style.top = "-140px";
-                            } else {
-                                dropdown_content_element.style.top = "30px";
-                            }
-
-                        }
+                        // let dropdown_content_elements = e.currentTarget.getElementsByClassName('answer_zones_dropdown_content');
+                        //
+                        // if (dropdown_content_elements.length === 1) {
+                        //     let container_top = document.getElementById('id_answers_collection').offsetTop;
+                        //     let container_height = document.getElementById('id_answers_collection').clientHeight;
+                        //     let button_top = e.currentTarget.offsetTop;
+                        //     let button_height = e.currentTarget.clientHeight;
+                        //     let dropdown_content_element = dropdown_content_elements[0];
+                        //     let menu_height = dropdown_content_element.clientHeight;
+                        //
+                        //     if ((button_top - container_top + button_height + menu_height) >= container_height) {
+                        //         dropdown_content_element.style.top = e.currentTarget.clientY + "px";//"-140px";
+                        //         dropdown_content_element.style.left = e.currentTarget.clientX + "px";
+                        //     } else {
+                        //         dropdown_content_element.style.top = e.currentTarget.clientY + "px";//"30px";
+                        //         dropdown_content_element.style.left = e.currentTarget.clientX + "px";
+                        //     }
+                        //
+                        // }
                     });
 
                     $('.answers_collection').sortable({
